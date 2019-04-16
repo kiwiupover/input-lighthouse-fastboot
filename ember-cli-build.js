27dbe0c1e7025935ec8ef8971beaee31b85707c5
Dave Laird <kiwiupover@gmail.com>
2019-04-15 19:48:05 -0700

    will it build

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,10 @@ const EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
   let app = new EmberApp(defaults, {
-
+    dotEnv: {
+      clientAllowedKeys: ['AUTOCOMPLETE_API_KEY'],
+      fastbootAllowedKeys: ['AUTOCOMPLETE_API_KEY']
+    }
   });
 
   return app.toTree();
